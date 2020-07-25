d4e59e46dd75eb3012362f5d7d1507e9457ed1e3
Noch4nce <m4rgera123@mail.ru>
2020-07-25 10:08:22 +0500

    logVal

---
+++ less 11-17/logVal.js
@@ -0,0 +1,29 @@
+// function getNum (num1, num2) {
+//     if (num1 === num2){
+//         console.log('ye')
+//     } else if (num1 !== num2) {
+//         console.log('no')
+//     } else {
+//         console.log('und')
+//     }
+// }
+// getNum(1, 1);
+
+// function getNum(num1, num2) {
+//     if ((num1 + num2) > 10){
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// console.log(getNum(12, 2));
+
+// function getNum (num) {
+//     if (num < 0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+// console.log(getNum(3));
