b1155c6e34f2eb57b14bf389fe5467a58d6f80bf
Noch4nce <m4rgera123@mail.ru>
2020-07-25 12:14:23 +0500

    pyramid

---
+++ less 11-17/pyramid.js
@@ -0,0 +1,51 @@
+// let str = '';
+// for (i = 1; i <= 9; i++){
+//     str += i;
+//     console.log(str);
+// }
+// console.log(str);
+
+//let str = '';
+// for (i = 9; i >= 1; i--){
+//     str += i;
+// }
+// console.log(str);
+
+// let str = '';
+// for (i = 1; i <= 9; i++){
+//     str += i; 
+// }
+// console.log(str.split().reverse().join());
+
+// let str = '';
+// for (i = 1; i <= 9; i++){
+//     str +='-' + i;
+// }
+// console.log(`${str}-`);
+
+// let str = '';
+// for (i = 1; i <= 20; i++){
+//     str += 'x'
+//     document.write(str + '<br>');
+// }
+
+// for (i = 1; i <= 9; i++){
+//     for (j = 1; j <= i; j++){
+//         for (k = 1; k <= 2; k++) {
+//             document.write(i);
+//         }  
+//     }
+//     document.write('<br>')
+// }
+
+// let str = 'x';
+// for (i = 1; i < 6; i++){
+//     for (j = 1; j <= i; j++){
+//         for (k = 1; k <= 2; k++){
+//             document.write(str); 
+//         }
+//     }
+//     document.write('<br>'); 
+// }
+
+
